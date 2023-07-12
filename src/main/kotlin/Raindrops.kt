307ac4e518c0a14688ce736e1062968f7348fcb6
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-07-12 08:57:03 +0300

    First iteration all tests are passing green

--- src/main/kotlin/Raindrops.kt
+++ src/main/kotlin/Raindrops.kt
@@ -1,6 +1,17 @@
 object Raindrops {
 
     fun convert(n: Int): String {
-        TODO("Implement this function to complete the task")
+        val iToWord = mapOf(3 to "Pling", 5 to "Plang", 7 to "Plong")
+
+        var result = iToWord.keys.fold("") { acc, k ->
+            if (n.mod(k) == 0) {
+                acc + iToWord[k]
+            } else {
+                acc
+            }
+        }
+        if (result == "") result = n.toString()
+
+        return result
     }
 }
